cfa4d73fc114079e3ccb5730911b2484cf2d9fb0
Imanol <jk199054@163.com>
2016-05-23 15:54:25 +0800

    show / delete png

--- PhotoSelector/PhotoSelector/PhotoSelectorViewController/PhotoSelectorViewController.swift
+++ PhotoSelector/PhotoSelector/PhotoSelectorViewController/PhotoSelectorViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 let photoSelectorCollectionViewCellIdentifer = "photoSelectorCollectionViewCellIdentifer"
 class PhotoSelectorViewController: UIViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -34,10 +36,36 @@ class PhotoSelectorViewController: UIViewController {
     //Mark:- lazy
     private lazy var collectionView : UICollectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: PhotoSelectorCellLayout())
     
+    private lazy var images = [UIImage]()
     
 }
 
+@objc
+protocol PhotoSelectorCellDelegate : NSObjectProtocol
+{
+    optional func photoSelectDidAdd(cell : PhotoSelectorCell)
+    optional func photoSelectDidRemove(cell : PhotoSelectorCell)
+}
+
 class PhotoSelectorCell : UICollectionViewCell {
+    
+    weak var delegate : PhotoSelectorCellDelegate?
+    
+    var image : UIImage?{
+        didSet{
+            if image == nil
+            {
+                addButton.setImage(UIImage(named: "compose_pic_add"), forState: UIControlState.Normal)
+                deleteButton.hidden = true
+                addButton.userInteractionEnabled = true
+            }else{
+                deleteButton.hidden = false
+                addButton.setImage(image, forState: UIControlState.Normal)
+                addButton.userInteractionEnabled = false
+            }
+            
+        }
+    }
     
     override init(frame: CGRect) {
         
@@ -65,25 +93,34 @@ class PhotoSelectorCell : UICollectionViewCell {
     }
     
     func deleteButtonClick(){
-        print(__FUNCTION__)
+        
+        if ((delegate?.respondsToSelector("photoSelectDidRemove:")) != false){
+            delegate?.photoSelectDidRemove!(self)
+        }
     }
     
     func addButtonClick(){
-        print(__FUNCTION__)
+        
+        if ((delegate?.respondsToSelector("photoSelectDidAdd:")) != false){
+            delegate?.photoSelectDidAdd!(self)
+        }
+        
     }
     
     //Mark:- lazy
+    
     private lazy var addButton : UIButton = {
        let btn = UIButton()
-        btn.setBackgroundImage(UIImage(named: "compose_pic_add"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: "compose_pic_add"), forState: UIControlState.Normal)
         btn.addTarget(self, action: "addButtonClick", forControlEvents: UIControlEvents.TouchUpInside)
         return btn
     }()
     
     private lazy var deleteButton : UIButton = {
         let btn = UIButton()
-        btn.setBackgroundImage(UIImage(named: "compose_photo_close"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: "compose_photo_close"), forState: UIControlState.Normal)
         btn.addTarget(self, action: "deleteButtonClick", forControlEvents: UIControlEvents.TouchUpInside)
+        btn.hidden = true
         return btn
     }()
     
@@ -92,16 +129,53 @@ class PhotoSelectorCell : UICollectionViewCell {
     }
 }
 
-extension PhotoSelectorViewController : UICollectionViewDataSource
+extension PhotoSelectorViewController : UICollectionViewDataSource,PhotoSelectorCellDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate
 {
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(photoSelectorCollectionViewCellIdentifer, forIndexPath: indexPath) as! PhotoSelectorCell
         cell.backgroundColor = UIColor.greenColor()
+        //cell.image = images[indexPath.item]
+        //这里image.count 的数目是固定的，而item 会逐渐+1 ， 因为numberOfItemsInSection这里已经控制了数量是比 image.count +1 ,而 index.item 是从0开始， 所以最后一个，就可以是 + 号图片
+        cell.image = indexPath.item == images.count ? nil : images[indexPath.item]
+        cell.delegate = self
         return cell
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return images.count + 1
+    }
+    
+    func photoSelectDidAdd(cell: PhotoSelectorCell) {
+        print(__FUNCTION__)
+        if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
+            print("can not open")
+            return
+        }
+        
+        let picVC = UIImagePickerController()
+        //tip: 除了需要继承UIImagePickerControllerDelegate之外，还需要继承UINavigationControllerDelegate，不然会报错
+        picVC.delegate = self
+        
+        // 设置允许用户编辑选中的图片
+        // 开发中如果需要上传头像, 那么请让用户编辑之后再上传
+        // 这样可以得到一张正方形的图片, 以便于后期处理(圆形)
+        picVC.allowsEditing = true
+        presentViewController(picVC, animated: true, completion: nil)
+    }
+    
+    func photoSelectDidRemove(cell: PhotoSelectorCell) {
+        //get index
+        let indexPath = collectionView.indexPathForCell(cell)
+        //remove item at index
+        images.removeAtIndex((indexPath?.item)!)
+        //reload
+        collectionView.reloadData()
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        images.append(image)
+        collectionView.reloadData()
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
